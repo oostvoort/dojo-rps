286f808f8d980d535a94825b49e01048b36cbaef
mariz <marrise@oostvoort.com>
2023-07-18 15:43:19 +0800

    feat: reveal when able

--- web/src/components/GamePanel.tsx
+++ web/src/components/GamePanel.tsx
@@ -1,27 +1,25 @@
-import React, {useState} from "react";
+import React from "react";
 import clsx from "clsx";
 import Choice from "./Choice.tsx";
 import useSaltGenerator from "../hooks/useSaltGenerator";
 import {useDojo} from "../DojoContext";
-import {commits, GAME_ID} from "../global/constants";
+import {commits, GAME_ID, PAPER, ROCK, SCISSORS, STATE_COMMIT_2, STATE_REVEAL_1} from "../global/constants";
 import {poseidonHashMany} from "micro-starknet";
 import {useComponentValue} from "@dojoengine/react";
 import {Utils} from "@dojoengine/core";
 import useLogs from "../hooks/useLogs.tsx";
 import useGameStatus from "../hooks/useGameStatus";
 
-// const gameId = 0
-
 export default function GamePanel() {
     const [selectedChoice, setSelectedChoice] = React.useState<string | null>(null)
-    const [remainingTime, setRemainingTime] = useState<number>(0)
+    const [remainingTime, setRemainingTime] = React.useState<number>(0)
     const {
         battleLogs,
         totalGames,
     } = useLogs()
 
     const {
-        systemCalls: { commit },
+        systemCalls: { commit, reveal },
         components: { Game, Player },
         network: { signer }
     } = useDojo()
@@ -33,13 +31,12 @@ export default function GamePanel() {
         changeSalt()
         const index = commits.findIndex(commit => choice === commit)
         const hashedCommit = poseidonHashMany([BigInt(index), BigInt(salt)])
-        commit(GAME_ID, hashedCommit).then((res) => {
-            console.log("COMMIT RESPONSE: ", res)
-        })
+        commit(GAME_ID, hashedCommit).then()
         setSelectedChoice(choice)
     }
 
     const game = useComponentValue(Game, Utils.getEntityIdFromKeys([BigInt(GAME_ID)]))
+    const gameStatus = game?.state ?? 0
 
     const isUserPlayer1 = BigInt(game?.player1 ?? 0) === BigInt(signer.address)
 
@@ -47,16 +44,19 @@ export default function GamePanel() {
     const player2Choice = game?.player2_commit ?? 0
 
     const opponentChoice = isUserPlayer1 ? player2Choice : player1Choice
+    const playerChoice = isUserPlayer1 ? player1Choice : player2Choice
 
     const statusText = useGameStatus()
     const player1 = useComponentValue(Player, Utils.getEntityIdFromKeys([BigInt(game?.player1 ?? 0)]))
     const player2 = useComponentValue(Player, Utils.getEntityIdFromKeys([BigInt(game?.player2 ?? 0)]))
 
-    console.log('game: ', game)
-    console.log('player 1: ', player1)
-    console.log('player 2: ', player2)
-    console.log("signer: ", signer)
-    console.log("salt: ", salt)
+    React.useEffect(() => {
+        if (gameStatus !== STATE_COMMIT_2 && gameStatus !== STATE_REVEAL_1) return
+        if (playerChoice) return
+        const index = commits.findIndex(commit => selectedChoice === commit) as (typeof ROCK | typeof PAPER | typeof SCISSORS)
+        const hashedCommit = poseidonHashMany([BigInt(index), BigInt(salt)])
+        reveal(GAME_ID, hashedCommit, index, salt)
+    }, [gameStatus, playerChoice])
 
     React.useEffect(() => {
         const interval = setInterval(() => {
@@ -103,7 +103,7 @@ export default function GamePanel() {
                     <div>
                         <div className={'mb-4 text-center'}>
                             <p className={'text-option-1 text-[28px] font-noto'}>Choose your hand</p>
-                            <p className={'text-option-2 text-[18px] font-noto'}>Times up in {remainingTime}...</p>
+                            <p className={'text-option-2 text-[18px] font-noto'}>Time's up in {remainingTime}...</p>
                             {/*<p className={'text-option-2 text-[36px] font-oswald'}>YOU WIN!</p>*/}
                         </div>
                         <div className={'flex gap-20'}>
@@ -132,20 +132,16 @@ export default function GamePanel() {
             <div className={'flex flex-col gap-6 w-3/12'}>
                 <div className={'p-8 rounded-3xl border-2 border-option-5 bg-option-6 text-start'}>
                     <p className={'text-[36px] text-option-2 font-oswald mb-2'}>Stats</p>\
-                    {/* TODO: keep track of total games played in use state*/}
                     <p className={'text-[18px] text-option-1 font-noto mb-1'}>Total Games: {totalGames}</p>
                     <p className={'text-[20px] text-option-2 font-bold'}>
-                        {/* TODO: hook up player1 wins*/}
                         Player1 : <span className={'text-option-1 ml-1'}>{player1?.wins ?? 0} Wins</span>
                     </p>
                     <p className={'text-[20px] text-option-2 font-bold'}>
-                        {/* TODO: hook up player2 wins*/}
                         Player2 : <span className={'text-option-1 ml-1'}>{player2?.wins ?? 0} Wins</span>
                     </p>
                 </div>
                 <div className={'p-8 h-full rounded-3xl border-2 border-option-5 bg-option-6 text-start'}>
                     <p className={'text-[36px] text-option-2 font-oswald mb-2'}>History</p>
-                    {/* TODO: store wins in useState */}
                     {
                         battleLogs.length > 0 && battleLogs.map((data, index) => {
                             return (
